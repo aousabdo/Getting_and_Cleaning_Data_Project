57183f77ea52508e589e767e18f11c639d1f72c3
aousabdo <aousabdo@gmail.com>
2014-04-27 01:11:35 -0400

    Repository containing project files

---
+++ run_Analysis.R
@@ -0,0 +1,45 @@
+library(data.table)
+
+## Read the activity labels file
+activity_names <- read.table("./data/UCI_HAR_Dataset/activity_labels.txt", header=FALSE)
+
+## Rename columns and modify column classes
+activity_names$V2     <- as.character(activity_names$V2)
+names(activity_names) <- c("Activity", "ActivityName")
+
+## Read the feature.txt file to extract the names for the columns in the data sets 
+feature_names <- read.table("./data/UCI_HAR_Dataset/features.txt", header=FALSE)
+
+## Rename columns and modify column classes
+feature_names$V2     <- as.character(feature_names$V2)
+names(feature_names) <- c("Feature", "FeatureName")
+
+## Read test data set
+testData      <- read.table("./data/UCI_HAR_Dataset/test/X_test.txt", header=FALSE)
+testData_labs <- read.table("./data/UCI_HAR_Dataset/test/y_test.txt", header=FALSE)
+testData_subj <- read.table("./data/UCI_HAR_Dataset/test/subject_test.txt", header=FALSE)
+
+## visual inspection that the length of the test data set is consistent
+cat(nrow(testData), nrow(testData_labs), nrow(testData_subj))
+
+## rename columns in the testData
+colnames(testData) <- feature_names$FeatureName
+
+## add new column which contains the name of the corresponding activity to the testData_labs dataframe
+testData_labs$V2 <- factor(testData_labs$V1, levels=activity_names$Activity, labels=activity_names$ActivityName)
+
+## now rename the columns in this dataframe
+names(testData_labs) <- c("Activity", "ActivityName")
+
+## rename column in testData_subj dataframe
+names(testData_subj) <- "Subject"
+
+
+##############------------- Now do the same for the training dataset ----------------#################
+## Read training data set
+trainData      <- read.table("./data/UCI_HAR_Dataset/train/X_train.txt", header=FALSE)
+trainData_labs <- read.table("./data/UCI_HAR_Dataset/train/y_train.txt", header=FALSE)
+trainData_subj <- read.table("./data/UCI_HAR_Dataset/train/subject_train.txt", header=FALSE)
+
+## visual inspection that the length of the traning data set is consistent 
+cat(nrow(trainData), nrow(trainData_labs), nrow(trainData_subj))
